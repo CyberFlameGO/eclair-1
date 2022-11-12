3b935a0fc22fb4b0edf69c88c03fbd7b6aba2db3
sstone <fabrice.drouin@acinq.fr>
2022-11-12 19:29:23 +0100

    Implement Bitcoin Core's external signer interface (HWI)

--- eclair-core/src/main/scala/fr/acinq/eclair/crypto/keymanager/LocalChannelKeyManager.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/crypto/keymanager/LocalChannelKeyManager.scala
@@ -17,18 +17,26 @@
 package fr.acinq.eclair.crypto.keymanager
 
 import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
+import fr.acinq.bitcoin.{Descriptor, ScriptWitness}
+import fr.acinq.bitcoin.psbt.{Psbt, SignPsbtResult, UpdateFailure}
 import fr.acinq.bitcoin.scalacompat.Crypto.{PrivateKey, PublicKey}
 import fr.acinq.bitcoin.scalacompat.DeterministicWallet._
 import fr.acinq.bitcoin.scalacompat.{Block, ByteVector32, ByteVector64, Crypto, DeterministicWallet}
+import fr.acinq.bitcoin.utils.EitherKt
 import fr.acinq.eclair.crypto.Generators
 import fr.acinq.eclair.crypto.Monitoring.{Metrics, Tags}
+import fr.acinq.eclair.crypto.keymanager.LocalChannelKeyManager.keyBasePath
 import fr.acinq.eclair.router.Announcements
-import fr.acinq.eclair.transactions.Transactions
+import fr.acinq.eclair.transactions.{Scripts, Transactions}
 import fr.acinq.eclair.transactions.Transactions.{CommitmentFormat, TransactionWithInputInfo, TxOwner}
 import fr.acinq.eclair.{KamonExt, randomLong}
 import grizzled.slf4j.Logging
 import kamon.tag.TagSet
+import kotlin.jvm.functions
 import scodec.bits.ByteVector
+
+import java.util
+import scala.jdk.CollectionConverters.MapHasAsScala
 
 object LocalChannelKeyManager {
   def keyBasePath(chainHash: ByteVector32): List[Long] = (chainHash: @unchecked) match {
@@ -154,4 +162,77 @@ class LocalChannelKeyManager(seed: ByteVector, chainHash: ByteVector32) extends 
 
   override def signChannelAnnouncement(witness: ByteVector, fundingKeyPath: KeyPath): ByteVector64 =
     Announcements.signChannelAnnouncement(witness, privateKeys.get(fundingKeyPath).privateKey)
+
+  override def getAccountPubKey(keyPath: KeyPath): ExtendedPublicKey = publicKey(derivePrivateKey(master, keyPath))
+
+  override def getDescriptors(): (List[String], List[String]) = {
+    val (keyPath: String, prefix: Int) = chainHash match {
+      case Block.RegtestGenesisBlock.hash | Block.TestnetGenesisBlock.hash => "84'/1'/0'/0" -> tpub
+      case Block.LivenetGenesisBlock.hash => "84'/0'/0'/0" -> xpub
+      case _ => throw new IllegalArgumentException(s"invalid chain hash ${chainHash}")
+    }
+    val accountPub = getAccountPubKey(KeyPath(keyPath))
+    val fingerprint = DeterministicWallet.fingerprint(master) & 0xFFFFFFFFL
+    val accountDesc = s"wpkh([${fingerprint.toHexString}/$keyPath]${encode(accountPub, prefix)}/0/*)"
+    val changeDesc = s"wpkh([${fingerprint.toHexString}/$keyPath]${encode(accountPub, prefix)}/1/*)"
+    (
+      List(s"$accountDesc#${Descriptor.checksum(accountDesc)}"),
+      List(s"$changeDesc#${Descriptor.checksum(changeDesc)}")
+    )
+  }
+
+  override def signPsbt(psbt: Psbt): Psbt = {
+    import fr.acinq.bitcoin.{SigHash, SigVersion, Script, Transaction}
+    import fr.acinq.bitcoin.scalacompat.KotlinUtils._
+
+    // check that outputs send either to one of our key or to a funding tx (a multisig 2-2 for which we have one of the 2 keys)
+    for (i <- 0 until psbt.getOutputs.size()) {
+      val output = psbt.getOutputs.get(i)
+      val txout = psbt.getGlobal.getTx.txOut.get(i)
+      output.getDerivationPaths.size() match {
+        case 2 =>
+          // check that this output is a multisig 2-2 funding tx output, and that we own one of the keys
+          var count = 0
+          val paths = output.getDerivationPaths.asScala.toList
+          paths.foreach { case (pub, keypath) =>
+            // add the chain-specific prefix
+            val prefix = KeyPath(keyBasePath(this.chainHash))
+            val path1 = prefix.keyPath.append(keypath.getKeyPath)
+            val priv = fr.acinq.bitcoin.DeterministicWallet.derivePrivateKey(master.priv, path1).getPrivateKey
+            val check = priv.publicKey()
+            if (pub == check) count = count + 1
+          }
+          //require(count >= 1)
+          val script = fr.acinq.bitcoin.scalacompat.Script.write(fr.acinq.bitcoin.scalacompat.Script.pay2wsh(Scripts.multiSig2of2(paths(0)._1, paths(1)._1)))
+          //assert(script == kmp2scala(txout.publicKeyScript))
+        case 1 =>
+          // change output, check that we own the key
+          output.getDerivationPaths.asScala.foreach { case (pub, keypath) =>
+            val priv = fr.acinq.bitcoin.DeterministicWallet.derivePrivateKey(master.priv, keypath.getKeyPath).getPrivateKey
+            val check = priv.publicKey()
+            require(pub == check)
+          }
+        case _ => () // TODO: check that this psbt is safe to sign
+      }
+    }
+    var psbt1 = psbt
+    for (pos <- 0 until psbt1.getInputs.size()) {
+      val input = psbt1.getInput(pos)
+      input.getDerivationPaths.asScala.foreach { case (pub, keypath) =>
+        val priv = fr.acinq.bitcoin.DeterministicWallet.derivePrivateKey(master.priv, keypath.getKeyPath).getPrivateKey
+        val check = priv.publicKey()
+        assert(check == pub)
+        assert(Script.isPay2wpkh(input.getWitnessUtxo.publicKeyScript.toByteArray))
+        val updated = psbt1.updateWitnessInput(psbt1.getGlobal.getTx.txIn.get(pos).outPoint, input.getWitnessUtxo, null, Script.pay2pkh(pub), SigHash.SIGHASH_ALL, input.getDerivationPaths)
+        val signed = EitherKt.flatMap(updated, (p: Psbt) => p.sign(priv, pos))
+        val finalized = EitherKt.flatMap(signed, (p: SignPsbtResult) => p.getPsbt.finalizeWitnessInput(pos, new ScriptWitness().push(p.getSig).push(pub.value)))
+        if (finalized.isLeft) {
+          throw new RuntimeException(finalized.getLeft.toString)
+        } else {
+          psbt1 = finalized.getRight
+        }
+      }
+    }
+    psbt1
+  }
 }
