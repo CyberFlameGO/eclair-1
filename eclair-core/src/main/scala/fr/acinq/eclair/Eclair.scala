remyers/eclair

Add support for paying offers

eclair-core/src/main/scala/fr/acinq/eclair/Eclair.scala
@@ -19,9 +19,9 @@ package fr.acinq.eclair
 import akka.actor.ActorRef
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.adapter.ClassicSchedulerOps
-import akka.actor.{ActorRef, typed}
-import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
-import akka.actor.typed.scaladsl.adapter.{ClassicActorSystemOps, ClassicSchedulerOps}
+import akka.actor.typed
+import akka.actor.typed.scaladsl.AskPattern.schedulerFromActorSystem
+import akka.actor.typed.scaladsl.adapter.ClassicActorSystemOps
 import akka.pattern._
 import akka.util.Timeout
 import com.softwaremill.quicklens.ModifyPimp
@@ -45,14 +45,16 @@ import fr.acinq.eclair.payment._
 import fr.acinq.eclair.payment.receive.MultiPartHandler.ReceivePayment
 import fr.acinq.eclair.payment.relay.Relayer.{GetOutgoingChannels, OutgoingChannels, RelayFees, UsableBalance}
 import fr.acinq.eclair.payment.send.MultiPartPaymentLifecycle.PreimageReceived
+import fr.acinq.eclair.payment.send.OfferPayment
 import fr.acinq.eclair.payment.send.PaymentInitiator._
 import fr.acinq.eclair.router.Router
 import fr.acinq.eclair.router.Router._
-import fr.acinq.eclair.wire.protocol.MessageOnionCodecs.blindedRouteCodec
+import fr.acinq.eclair.wire.protocol.MessageOnionCodecs.{blindedRouteCodec, perHopPayloadCodec}
+import fr.acinq.eclair.wire.protocol.Offers.Offer
 import fr.acinq.eclair.wire.protocol._
 import grizzled.slf4j.Logging
 import scodec.bits.ByteVector
-import scodec.{Attempt, DecodeResult, codecs}
+import scodec.{Attempt, DecodeResult}
 
 import java.nio.charset.StandardCharsets
 import java.util.UUID
@@ -162,6 +164,8 @@ trait Eclair {
   def verifyMessage(message: ByteVector, recoverableSignature: ByteVector): VerifiedMessage
 
   def sendOnionMessage(intermediateNodes: Seq[PublicKey], destination: Either[PublicKey, Sphinx.RouteBlinding.BlindedRoute], replyPath: Option[Seq[PublicKey]], userCustomContent: ByteVector)(implicit timeout: Timeout): Future[SendOnionMessageResponse]
+
+  def payOffer(offer: Offer, amount: MilliSatoshi, quantity: Long, externalId_opt: Option[String] = None, maxAttempts_opt: Option[Int] = None, maxFeeFlat_opt: Option[Satoshi] = None, maxFeePct_opt: Option[Double] = None, pathFindingExperimentName_opt: Option[String] = None)(implicit timeout: Timeout): Future[PayOfferResponse]
 }
 
 class EclairImpl(appKit: Kit) extends Eclair with Logging {
@@ -542,7 +546,7 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
                                 destination: Either[PublicKey, Sphinx.RouteBlinding.BlindedRoute],
                                 replyPath: Option[Seq[PublicKey]],
                                 userCustomContent: ByteVector)(implicit timeout: Timeout): Future[SendOnionMessageResponse] = {
-    codecs.list(TlvCodecs.genericTlv).decode(userCustomContent.bits) match {
+    perHopPayloadCodec.decode(userCustomContent.bits) match {
       case Attempt.Successful(DecodeResult(userCustomTlvs, _)) =>
         val replyPathId = randomBytes32()
         val replyRoute = replyPath.map(hops => OnionMessages.buildRoute(randomKey(), hops.map(OnionMessages.IntermediateNode(_)), OnionMessages.Recipient(appKit.nodeParams.nodeId, Some(replyPathId))))
@@ -554,7 +558,7 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
           replyRoute.map(OnionMessagePayloadTlv.ReplyPath(_) :: Nil).getOrElse(Nil),
           userCustomTlvs)
         appKit.postman.ask(ref => Postman.SendMessage(nextNodeId, message, replyPath.map(_ => replyPathId), ref, appKit.nodeParams.onionMessageConfig.timeout))(timeout, appKit.system.scheduler.toTyped).mapTo[Postman.OnionMessageResponse].map {
-          case Postman.Response(payload) =>
+          case Postman.Response(payload, _) =>
             val encodedReplyPath = payload.replyPath.map(route => blindedRouteCodec.encode(route.blindedRoute).require.bytes.toHex)
             SendOnionMessageResponse(sent = true, None, Some(SendOnionMessageResponsePayload(encodedReplyPath, payload.replyPath.map(_.blindedRoute), payload.records.unknown.map(tlv => tlv.tag.toString -> tlv.value).toMap)))
           case Postman.NoReply => SendOnionMessageResponse(sent = true, Some("No response"), None)
@@ -562,6 +566,43 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
           case Postman.SendingStatus(failure: MessageRelay.Failure) => SendOnionMessageResponse(sent = false, Some(failure.toString), None)
         }
       case Attempt.Failure(cause) => Future.successful(SendOnionMessageResponse(sent = false, failureMessage = Some(s"the `content` field is invalid, it must contain encoded tlvs: ${cause.message}"), response = None))
+    }
+  }
+
+  override def payOffer(offer: Offer,
+                        amount: MilliSatoshi,
+                        quantity: Long,
+                        externalId_opt: Option[String],
+                        maxAttempts_opt: Option[Int],
+                        maxFeeFlat_opt: Option[Satoshi],
+                        maxFeePct_opt: Option[Double],
+                        pathFindingExperimentName_opt: Option[String])(implicit timeout: Timeout): Future[PayOfferResponse] = {
+    if(externalId_opt.exists(_.length > externalIdMaxLength)) {
+      return Future.failed(new IllegalArgumentException(s"externalId is too long: cannot exceed $externalIdMaxLength characters"))
+    }
+    val routeParams = getRouteParams(pathFindingExperimentName_opt) match {
+      case Right(defaultRouteParams) =>
+        defaultRouteParams
+          .modify(_.boundaries.maxFeeProportional).setToIfDefined(maxFeePct_opt.map(_ / 100))
+          .modify(_.boundaries.maxFeeFlat).setToIfDefined(maxFeeFlat_opt.map(_.toMilliSatoshi))
+      case Left(t) => return Future.failed(t)
+    }
+    val sendPaymentConfig = OfferPayment.SendPaymentConfig(externalId_opt, maxAttempts_opt.getOrElse(appKit.nodeParams.maxPaymentAttempts), routeParams)
+    val offerPayment = appKit.system.spawnAnonymous(OfferPayment(appKit.nodeParams, appKit.postman, appKit.paymentInitiator, offer, amount, quantity, sendPaymentConfig))
+    offerPayment.ask((ref: typed.ActorRef[OfferPayment.Result]) => OfferPayment.PayOffer(ref)).mapTo[OfferPayment.Result].map {
+      case OfferPayment.Success(invoice, payerKey, paymentPreimage) => PayOfferResponse(Some(invoice.write), None, Some(payerKey.value.toHex), Some(paymentPreimage.toHex), None)
+      case OfferPayment.UnsupportedFeatures(features) => PayOfferResponse(None, None, None, None, Some(s"Unsupported features: $features"))
+      case OfferPayment.UnsupportedChains(chains) => PayOfferResponse(None, None, None, None, Some(s"Unsupported chains: ${chains.mkString(",")}"))
+      case OfferPayment.UnsupportedCurrency(iso4217) => PayOfferResponse(None, None, None, None, Some(s"Unsupported currency: $iso4217"))
+      case OfferPayment.ExpiredOffer(expiryDate) => PayOfferResponse(None, None, None, None, Some(s"Offer expired since $expiryDate"))
+      case OfferPayment.QuantityTooLow(quantityMin) => PayOfferResponse(None, None, None, None, Some(s"Minimum quantity is $quantityMin"))
+      case OfferPayment.QuantityTooHigh(quantityMax) => PayOfferResponse(None, None, None, None, Some(s"Maximum quantity is $quantityMax"))
+      case OfferPayment.IsSendInvoice => PayOfferResponse(None, None, None, None, Some(""))
+      case OfferPayment.AmountInsufficient(amountNeeded) => PayOfferResponse(None, None, None, None, Some(s"Paying this offer requires at least $amountNeeded"))
+      case OfferPayment.InvalidSignature(signature) => PayOfferResponse(None, None, None, None, Some(s"Invalid signature: ${signature.toHex}"))
+      case OfferPayment.InvalidInvoice(invoice, request, payerKey) => PayOfferResponse(Some(invoice.write), Some(request.encode), Some(payerKey.value.toHex), None, Some("Invalid invoice"))
+      case OfferPayment.NoInvoice => PayOfferResponse(None, None, None, None, Some("Couldn't retrieve invoice"))
+      case OfferPayment.FailedPayment(invoice, payerKey, paymentFailed) => PayOfferResponse(Some(invoice.write), None, Some(payerKey.value.toHex), None, Some(paymentFailed.toString))
     }
   }
 }

eclair-node/src/main/scala/fr/acinq/eclair/api/directives/ExtraDirectives.scala
@@ -27,6 +27,7 @@ import fr.acinq.eclair.ApiTypes.ChannelIdentifier
 import fr.acinq.eclair.api.serde.FormParamExtractors._
 import fr.acinq.eclair.api.serde.JsonSupport._
 import fr.acinq.eclair.payment.Bolt11Invoice
+import fr.acinq.eclair.wire.protocol.Offers.Offer
 import fr.acinq.eclair.{MilliSatoshi, ShortChannelId, TimestampSecond}
 
 import scala.concurrent.Future
@@ -50,6 +51,7 @@ trait ExtraDirectives extends Directives {
   val ignoreNodeIdsFormParam: NameUnmarshallerReceptacle[List[PublicKey]] = "ignoreNodeIds".as[List[PublicKey]](pubkeyListUnmarshaller)
   val ignoreShortChannelIdsFormParam: NameUnmarshallerReceptacle[List[ShortChannelId]] = "ignoreShortChannelIds".as[List[ShortChannelId]](shortChannelIdsUnmarshaller)
   val maxFeeMsatFormParam: NameReceptacle[MilliSatoshi] = "maxFeeMsat".as[MilliSatoshi]
+  val offerFormParam: NameUnmarshallerReceptacle[Offer] = "offer".as[Offer](offerUnmarshaller)
 
   // custom directive to fail with HTTP 404 (and JSON response) if the element was not found
   def completeOrNotFound[T](fut: Future[Option[T]])(implicit marshaller: ToResponseMarshaller[T]): Route = onComplete(fut) {

eclair-node/src/main/scala/fr/acinq/eclair/api/serde/FormParamExtractors.scala
@@ -27,6 +27,7 @@ import fr.acinq.eclair.crypto.Sphinx
 import fr.acinq.eclair.io.NodeURI
 import fr.acinq.eclair.payment.Bolt11Invoice
 import fr.acinq.eclair.wire.protocol.MessageOnionCodecs.blindedRouteCodec
+import fr.acinq.eclair.wire.protocol.Offers.Offer
 import fr.acinq.eclair.{MilliSatoshi, ShortChannelId, TimestampSecond}
 import scodec.bits.ByteVector
 
@@ -73,6 +74,8 @@ object FormParamExtractors {
   val blindedRouteUnmarshaller: Unmarshaller[String, Sphinx.RouteBlinding.BlindedRoute] = Unmarshaller.strict { str =>
     blindedRouteCodec.decode(ByteVector.fromValidHex(str).bits).require.value
   }
+
+  val offerUnmarshaller: Unmarshaller[String, Offer] = Unmarshaller.strict { Offer.decode(_).get }
 
   private def listUnmarshaller[T](unmarshal: String => T): Unmarshaller[String, List[T]] = Unmarshaller.strict { str =>
     Try(serialization.read[List[String]](str).map(unmarshal))

CyberFlameGO/eclair-1

eclair-core/src/main/scala/fr/acinq/eclair/Eclair.scala
@@ -16,13 +16,13 @@
 
 package fr.acinq.eclair
 
-import akka.actor.ActorRef
-import akka.actor.typed.scaladsl.AskPattern.Askable
-import akka.actor.typed.scaladsl.adapter.ClassicSchedulerOps
+import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import akka.actor.typed.scaladsl.adapter.{ClassicActorSystemOps, ClassicSchedulerOps}
+import akka.actor.{ActorRef, typed}
 import akka.pattern._
 import akka.util.Timeout
 import com.softwaremill.quicklens.ModifyPimp
-import fr.acinq.bitcoin.scalacompat.Crypto.PublicKey
+import fr.acinq.bitcoin.scalacompat.Crypto.{PrivateKey, PublicKey}
 import fr.acinq.bitcoin.scalacompat.{ByteVector32, ByteVector64, Crypto, Satoshi}
 import fr.acinq.eclair.ApiTypes.ChannelNotFound
 import fr.acinq.eclair.balance.CheckBalance.GlobalBalance
@@ -42,14 +42,16 @@ import fr.acinq.eclair.payment._
 import fr.acinq.eclair.payment.receive.MultiPartHandler.ReceiveStandardPayment
 import fr.acinq.eclair.payment.relay.Relayer.{ChannelBalance, GetOutgoingChannels, OutgoingChannels, RelayFees}
 import fr.acinq.eclair.payment.send.MultiPartPaymentLifecycle.PreimageReceived
+import fr.acinq.eclair.payment.send.OfferPayment
 import fr.acinq.eclair.payment.send.PaymentInitiator._
 import fr.acinq.eclair.router.Router
 import fr.acinq.eclair.router.Router._
 import fr.acinq.eclair.wire.protocol.MessageOnionCodecs.blindedRouteCodec
+import fr.acinq.eclair.wire.protocol.OfferTypes.{InvoiceRequest, Offer}
 import fr.acinq.eclair.wire.protocol._
 import grizzled.slf4j.Logging
 import scodec.bits.ByteVector
-import scodec.{Attempt, DecodeResult, codecs}
+import scodec.{Attempt, DecodeResult}
 
 import java.nio.charset.StandardCharsets
 import java.util.UUID
@@ -68,6 +70,8 @@ case class VerifiedMessage(valid: Boolean, publicKey: PublicKey)
 
 case class SendOnionMessageResponsePayload(encodedReplyPath: Option[String], replyPath: Option[Sphinx.RouteBlinding.BlindedRoute], unknownTlvs: Map[String, ByteVector])
 case class SendOnionMessageResponse(sent: Boolean, failureMessage: Option[String], response: Option[SendOnionMessageResponsePayload])
+
+case class PayOfferResponse(invoiceRequest: Option[InvoiceRequest], payerKey: Option[PrivateKey], invoice: Option[Bolt12Invoice], paymentId: Option[UUID], outgoingPayment: Seq[OutgoingPayment])
 // @formatter:on
 
 object SignedMessage {
@@ -163,12 +167,18 @@ trait Eclair {
 
   def sendOnionMessage(intermediateNodes: Seq[PublicKey], destination: Either[PublicKey, Sphinx.RouteBlinding.BlindedRoute], replyPath: Option[Seq[PublicKey]], userCustomContent: ByteVector)(implicit timeout: Timeout): Future[SendOnionMessageResponse]
 
+  def payOffer(offer: Offer, amount: MilliSatoshi, quantity: Long, externalId_opt: Option[String] = None, maxAttempts_opt: Option[Int] = None, maxFeeFlat_opt: Option[Satoshi] = None, maxFeePct_opt: Option[Double] = None, pathFindingExperimentName_opt: Option[String] = None)(implicit timeout: Timeout): Future[UUID]
+
+  def payOfferStatus(id: Either[UUID, Offer])(implicit timeout: Timeout): Future[Seq[PayOfferResponse]]
+
   def stop(): Future[Unit]
 }
 
 class EclairImpl(appKit: Kit) extends Eclair with Logging {
 
   implicit val ec: ExecutionContext = appKit.system.dispatcher
+
+  implicit val typedSystem: typed.ActorSystem[Nothing] = appKit.system.toTyped
 
   // We constrain external identifiers. This allows uuid, long and pubkey to be used.
   private val externalIdMaxLength = 66
@@ -350,7 +360,7 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
         externalId_opt match {
           case Some(externalId) if externalId.length > externalIdMaxLength => Left(new IllegalArgumentException(s"externalId is too long: cannot exceed $externalIdMaxLength characters"))
           case _ if invoice.isExpired() => Left(new IllegalArgumentException("invoice has expired"))
-          case _ => Right(SendPaymentToNode(amount, invoice, maxAttempts, externalId_opt, routeParams = routeParams))
+          case _ => Right(SendPaymentToNode(ActorRef.noSender, amount, invoice, maxAttempts, externalId_opt, routeParams = routeParams))
         }
       case Left(t) => Left(t)
     }
@@ -551,7 +561,7 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
                                 destination: Either[PublicKey, Sphinx.RouteBlinding.BlindedRoute],
                                 replyPath: Option[Seq[PublicKey]],
                                 userCustomContent: ByteVector)(implicit timeout: Timeout): Future[SendOnionMessageResponse] = {
-    codecs.list(TlvCodecs.genericTlv).decode(userCustomContent.bits) match {
+    MessageOnionCodecs.perHopPayloadCodec.decode(userCustomContent.bits) match {
       case Attempt.Successful(DecodeResult(userCustomTlvs, _)) =>
         val replyPathId = randomBytes32()
         val replyRoute = replyPath.map(hops => OnionMessages.buildRoute(randomKey(), hops.map(OnionMessages.IntermediateNode(_)), OnionMessages.Recipient(appKit.nodeParams.nodeId, Some(replyPathId))))
@@ -575,6 +585,46 @@ class EclairImpl(appKit: Kit) extends Eclair with Logging {
         }
       case Attempt.Failure(cause) => Future.successful(SendOnionMessageResponse(sent = false, failureMessage = Some(s"the `content` field is invalid, it must contain encoded tlvs: ${cause.message}"), response = None))
     }
+  }
+
+  override def payOffer(offer: Offer,
+                        amount: MilliSatoshi,
+                        quantity: Long,
+                        externalId_opt: Option[String],
+                        maxAttempts_opt: Option[Int],
+                        maxFeeFlat_opt: Option[Satoshi],
+                        maxFeePct_opt: Option[Double],
+                        pathFindingExperimentName_opt: Option[String])(implicit timeout: Timeout): Future[UUID] = {
+    if (externalId_opt.exists(_.length > externalIdMaxLength)) {
+      return Future.failed(new IllegalArgumentException(s"externalId is too long: cannot exceed $externalIdMaxLength characters"))
+    }
+    val routeParams = getRouteParams(pathFindingExperimentName_opt) match {
+      case Right(defaultRouteParams) =>
+        defaultRouteParams
+          .modify(_.boundaries.maxFeeProportional).setToIfDefined(maxFeePct_opt.map(_ / 100))
+          .modify(_.boundaries.maxFeeFlat).setToIfDefined(maxFeeFlat_opt.map(_.toMilliSatoshi))
+      case Left(t) => return Future.failed(t)
+    }
+    val sendPaymentConfig = OfferPayment.SendPaymentConfig(externalId_opt, maxAttempts_opt.getOrElse(appKit.nodeParams.maxPaymentAttempts), routeParams)
+    val offerPayment = appKit.system.spawnAnonymous(OfferPayment(appKit.nodeParams, appKit.postman, appKit.paymentInitiator, offer, amount, quantity, sendPaymentConfig))
+    offerPayment.ask((ref: typed.ActorRef[OfferPayment.Result]) => OfferPayment.PayOffer(ref)).mapTo[OfferPayment.Result].flatMap {
+      case f: OfferPayment.Failure => Future.failed(f)
+      case OfferPayment.Processing(uuid) => Future.successful(uuid)
+    }
+  }
+
+  override def payOfferStatus(id: Either[UUID, Offer])(implicit timeout: Timeout): Future[Seq[PayOfferResponse]] = {
+    Future {
+      id match {
+        case Left(uuid) => appKit.nodeParams.db.offers.getAttemptToPayOffer(uuid).toSeq
+        case Right(offer) => appKit.nodeParams.db.offers.getAttemptsToPayOffer(offer)
+      }
+    }.flatMap(attempts => Future.sequence(attempts.map(payOfferAttempt => {
+      payOfferAttempt.paymentId_opt match {
+        case Some(paymentId) => sentInfo(Left(paymentId)).map(outgoingPayments => PayOfferResponse(Some(payOfferAttempt.request), Some(payOfferAttempt.payerKey), payOfferAttempt.invoice, Some(paymentId), outgoingPayments))
+        case None => Future.successful(PayOfferResponse(Some(payOfferAttempt.request), Some(payOfferAttempt.payerKey), payOfferAttempt.invoice, None, Nil))
+      }
+    })))
   }
 
   override def stop(): Future[Unit] = {

eclair-node/src/main/scala/fr/acinq/eclair/api/directives/ExtraDirectives.scala
@@ -27,6 +27,7 @@ import fr.acinq.eclair.ApiTypes.ChannelIdentifier
 import fr.acinq.eclair.api.serde.FormParamExtractors._
 import fr.acinq.eclair.api.serde.JsonSupport._
 import fr.acinq.eclair.payment.Bolt11Invoice
+import fr.acinq.eclair.wire.protocol.OfferTypes.Offer
 import fr.acinq.eclair.{MilliSatoshi, Paginated, ShortChannelId, TimestampSecond}
 
 import scala.concurrent.Future
@@ -54,6 +55,7 @@ trait ExtraDirectives extends Directives {
   val maxFeeMsatFormParam: NameReceptacle[MilliSatoshi] = "maxFeeMsat".as[MilliSatoshi]
   val countFormParam: NameReceptacle[Int] = "count".as[Int]
   val skipFormParam: NameReceptacle[Int] = "skip".as[Int]
+  val offerFormParam: NameUnmarshallerReceptacle[Offer] = "offer".as[Offer](offerUnmarshaller)
 
   // custom directive to fail with HTTP 404 (and JSON response) if the element was not found
   def completeOrNotFound[T](fut: Future[Option[T]])(implicit marshaller: ToResponseMarshaller[T]): Route = onComplete(fut) {

eclair-node/src/main/scala/fr/acinq/eclair/api/serde/FormParamExtractors.scala
@@ -27,6 +27,7 @@ import fr.acinq.eclair.crypto.Sphinx
 import fr.acinq.eclair.io.NodeURI
 import fr.acinq.eclair.payment.Bolt11Invoice
 import fr.acinq.eclair.wire.protocol.MessageOnionCodecs.blindedRouteCodec
+import fr.acinq.eclair.wire.protocol.OfferTypes.Offer
 import fr.acinq.eclair.{MilliSatoshi, ShortChannelId, TimestampSecond}
 import scodec.bits.ByteVector
 
@@ -73,6 +74,8 @@ object FormParamExtractors {
   val blindedRouteUnmarshaller: Unmarshaller[String, Sphinx.RouteBlinding.BlindedRoute] = Unmarshaller.strict { str =>
     blindedRouteCodec.decode(ByteVector.fromValidHex(str).bits).require.value
   }
+
+  val offerUnmarshaller: Unmarshaller[String, Offer] = Unmarshaller.strict { Offer.decode(_).get }
 
   private def listUnmarshaller[T](unmarshal: String => T): Unmarshaller[String, List[T]] = Unmarshaller.strict { str =>
     Try(serialization.read[List[String]](str).map(unmarshal))